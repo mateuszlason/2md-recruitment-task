6b247079c05ac00403d48fda8dd26c22343a7fbb
Mateusz Lasoń <crew1997@gmail.com>
2021-05-31 22:21:37 +0200

    fixed the footer, (must have accidentally removed styles while deploying)

--- src/components/FooterColumn.js
+++ src/components/FooterColumn.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FooterColumn = ({ headerText, children }) => {
   return (
-    <div className="flex flex-col md:my-3 md:ml-0 xl:ml-8">
+    <div className="flex flex-col md:my-3 md:ml-0 xl:ml-8 md:w-3/4 w-1/4">
       <h2 className="text-sm mb-5">{headerText}</h2>
       {children}
     </div>
